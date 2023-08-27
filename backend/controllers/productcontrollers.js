aa55a6349fd832e06f7a823acd47fc69f588251f
Wasif007 <wasifateeq007@hotmail.com>
2023-08-27 11:12:08 +0500

    fetching all products route created

--- backend/controllers/productcontrollers.js
+++ backend/controllers/productcontrollers.js
@@ -1,7 +1,7 @@
 const productSchema=require("../models/productModel");
 
 
-//Route to add a new Product acc to Schema
+//Route to add a new Product acc to Schema --Admin
 exports.addingAProduct=async(req,res)=>{
 const productCreatedViaReq=await productSchema.create(req.body);
 res.status(201).json({success:true,productCreatedViaReq});
@@ -9,7 +9,7 @@ res.status(201).json({success:true,productCreatedViaReq});
 
 
 //Route of all products made here
-exports.getAllProducts=(req,res)=>{
-    console.log("Hello");
-res.status(200).json({success:"Route created of all products"});
+exports.getAllProducts=async(req,res)=>{
+   const fetchAllProducts =await productSchema.find();
+res.status(200).json({success:true,fetchAllProducts});
 }
