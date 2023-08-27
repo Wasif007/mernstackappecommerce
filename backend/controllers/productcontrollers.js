6172351a44009f33a6406c9d117cf9af76175e53
Wasif007 <wasifateeq007@hotmail.com>
2023-08-27 11:38:56 +0500

    update route made

--- backend/controllers/productcontrollers.js
+++ backend/controllers/productcontrollers.js
@@ -13,3 +13,26 @@ exports.getAllProducts=async(req,res)=>{
    const fetchAllProducts =await productSchema.find();
 res.status(200).json({success:true,fetchAllProducts});
 }
+
+//Route of updating a product here --Admin
+exports.updatingAProduct=async(req,res)=>{
+    try {
+         //Finding a product through id given in url
+    let productFinding=await productSchema.findById(req.params.id);
+     if(!productFinding){
+       
+     return res.status(500).json({"success":false,"message":"Product Not found with that id"});
+    }
+    //Id  not found than send false message
+    } catch (error) {
+        console.log(error);
+    }
+   
+   
+    //If Id is found than update it with request body values
+    productFinding=await productSchema.findByIdAndUpdate(req.params.id,req.body,{new:true,runValidators:true,useFindAndModify:false});
+    res.status(200).json({
+        success:true,
+        productFinding
+    })
+}
--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,10 +1,12 @@
 const express=require("express");
 //Importing all controllers for products 
-const { getAllProducts, addingAProduct} = require("../controllers/productcontrollers");
+const { getAllProducts, addingAProduct, updatingAProduct} = require("../controllers/productcontrollers");
 
 const router=express.Router();
 //Making all routes for product CRUD
 router.route("/products").get(getAllProducts);
 router.route("/new/product").post(addingAProduct);
+router.route("/product/:id").put(updatingAProduct);
+
 
 module.exports=router
