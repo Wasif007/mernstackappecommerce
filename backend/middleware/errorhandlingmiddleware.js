e45e73e87bf3d44cd9bfe02fbaa31c7df4f2f24c
Wasif007 <wasifateeq007@hotmail.com>
2023-08-27 23:31:32 +0500

    error handling on server side done

--- backend/middleware/errorhandlingmiddleware.js
+++ backend/middleware/errorhandlingmiddleware.js
@@ -3,8 +3,13 @@ const importingUtilsError=require("../Utils/errorHandling");
 module.exports=(err,req,res,next)=>{
     err.statusCode=err.statusCode||500;
     err.message=err.message||"Internal Server Error";
+
+    if(err.name==="Casterror"){
+        const message=`Invalid Resource found ${err.path}`;
+        err=new importingUtilsError(message,400);
+    }
     res.status(err.statusCode).json({
         success:false,
-        error:err
+        message:err.message
     })
 }
--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,13 @@ const app=require("./app");
 const dotenv=require("dotenv");
 //Importing DataBase
 const database=require("./config/database");
+//UnCaught Exception errors
+process.on("uncaughtException",(error)=>{
+    console.log(`Error is ${error.message}`);
+    console.log("Shutting down Server");
+        process.exit(1);
+   
+})
 
 //Configuring a file
 dotenv.config({path:'backend/config/config.env'});
