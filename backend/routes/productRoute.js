c7b34b73eed7e07aec1ebb5a28c69d2d41dd897a
Wasif007 <wasifateeq007@hotmail.com>
2023-09-03 15:29:13 +0500

    refactoring acc to simple user and admin

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -7,9 +7,9 @@ const isRoleDefined = require("../middleware/roleDefinedAuth");
 const router=express.Router();
 
 //Making all routes for product CRUD
-router.route("/products").get(isAuthenticatedUser,isRoleDefined("admin"),getAllProducts);
-router.route("/new/product").post(isAuthenticatedUser,isRoleDefined("admin"),addingAProduct);
-router.route("/product/:id").put(isAuthenticatedUser,isRoleDefined("admin"),updatingAProduct).delete(isAuthenticatedUser,isRoleDefined("admin"),deleteAProduct).get(findingASProduct);
-
+router.route("/admin/products").get(isAuthenticatedUser,isRoleDefined("admin"),getAllProducts);
+router.route("/admin/new/product").post(isAuthenticatedUser,isRoleDefined("admin"),addingAProduct);
+router.route("/admin/product/:id").put(isAuthenticatedUser,isRoleDefined("admin"),updatingAProduct).delete(isAuthenticatedUser,isRoleDefined("admin"),deleteAProduct);
+router.route("/product/:id").get(findingASProduct)
 
 module.exports=router
