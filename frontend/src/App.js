0573e19f04bc712f2b9a57b77ebcf27ca6d618e5
Wasif007 <wasifateeq007@hotmail.com>
2023-09-08 00:13:57 +0500

    header and footer broiler plate set

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,24 @@
 
+import { useEffect } from 'react';
 import './App.css';
-import Header from "./component/layout/header.js"
+import Header from "./component/layout/Header/header.js"
 import {BrowserRouter as Router} from "react-router-dom"
+import WebFont from "webfontloader";
+import Footer from "./component/layout/Footer/footer"
 function App() {
+  useEffect(() => {
+    WebFont.load({
+      google: {
+        families: ["Roboto", "Droid Sans", "Chilanka"],
+      },
+    });
+
+   
+  }, []);
   return (
    <Router>
       <Header />
+      <Footer/>
     </Router>
   );
 }
--- frontend/src/component/layout/header.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import {ReactNavbar} from "overlay-navbar"
-const header = () => {
-  return (
-    <ReactNavbar/>
-  )
-}
-
-export default header
