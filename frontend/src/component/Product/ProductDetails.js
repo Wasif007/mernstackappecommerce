3c1e7be4096bfbdac6e0d03a865f6b97a20ce99b
Wasif007 <wasifateeq007@hotmail.com>
2023-09-10 11:47:45 +0500

    single route product front end working

--- frontend/src/component/Product/ProductDetails.js
+++ frontend/src/component/Product/ProductDetails.js
@@ -1,10 +1,34 @@
-import React from 'react'
-
+import React, { Fragment, useEffect } from 'react'
+import {CgCarousel} from "react-material-ui-carousel"
+import { useDispatch,useSelector } from 'react-redux'
+import { getSingleProductAdmin } from '../../actions/productAction'
 const ProductDetails = () => {
+    const dispatch=useDispatch();
+    useEffect(() => {
+        dispatch(getSingleProductAdmin());
+      }, [dispatch]);
+      const {product,error,loading,productCount}=useSelector(state=>state.ProductDetails);
+
   return (
-    <div>
-      Hello wordl
-    </div>
+    <Fragment>
+       <div className='ProductDetails'>
+       <div>
+            <CgCarousel>
+        {
+            product.images && product.images.map((img,i)=>(
+                <img className='CarouselImage'
+                key={img.url}
+                src={img.url}
+                alt={`${i} Slide`}
+                />
+                
+                
+            ))
+        }
+            </CgCarousel>
+        </div>
+       </div>
+    </Fragment>
   )
 }
 
--- frontend/src/reducers/productReducer.js
+++ frontend/src/reducers/productReducer.js
@@ -32,31 +32,30 @@ error:null
 
 };
 //Single product get reducer
-export const oneProductReducer=(state={product:{}},action)=>{
+export const oneProductReducer = (state = { productDetails: {} }, action) => {
     switch (action.type) {
-        case ONE_PRODUCT_SUCCESS:
-            return{
-                loading:false,
-                product:action.payload.fetchAllProducts,
-            }
-            case ONE_PRODUCT_FAIL:
-            return{
-                loading:false,
-                error:action.payload
-            }
-            case ONE_PRODUCT_REQUEST:
-            return{
-                loading:true,
-                ...state
-            }
-            
-            case ALL_ERROR_CLEAR:
-            return{
-    ...state,
-    error:null
-            }
-        default:
-            return state;
+      case ONE_PRODUCT_REQUEST:
+        return {
+          loading: true,
+          ...state,
+        };
+      case ONE_PRODUCT_SUCCESS:
+        return {
+          loading: false,
+          productDetails: action.payload.productTFind,
+        };
+      case ONE_PRODUCT_FAIL:
+        return {
+          loading: false,
+          error: action.payload,
+        };
+  
+      case ALL_ERROR_CLEAR:
+        return {
+          ...state,
+          error: null,
+        };
+      default:
+        return state;
     }
-    
-    };
+  };
