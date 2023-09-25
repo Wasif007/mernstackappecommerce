703ba92be1fb329abdc25ff437a99404eddf87a7
Wasif007 <wasifateeq007@hotmail.com>
2023-09-26 00:00:05 +0500

    No product in cart page updated

--- frontend/src/component/Home/Cart/Cart.js
+++ frontend/src/component/Home/Cart/Cart.js
@@ -3,6 +3,10 @@ import "./Cart.css"
 import CartItemCard from './CartItemCard'
 import {useSelector,useDispatch} from "react-redux"
 import { addtoCart, removeFromCart } from '../../../actions/cartActions'
+import { Link } from 'react-router-dom'
+import RemoveShoppingCartIcon from '@mui/icons-material/RemoveShoppingCart';
+
+
 
 const Cart = () => {
     const dispatch=useDispatch();
@@ -29,41 +33,51 @@ const deleteProductFCart=(id)=>{
 dispatch(removeFromCart(id));
 }
   return (
-    <Fragment>
-    <div className="cartPage">
-      <div className="cartHeader">
-        <p>Product</p>
-        <p>Quantity</p>
-        <p>Subtotal</p>
+   <Fragment>
+    {
+        cartItem.length===0?
+        <div className="emptyCart">
+        <RemoveShoppingCartIcon/>
+        <p>No Product in Your Cart</p>
+        <Link to="/products">View Products</Link>
       </div>
-      {
-        cartItem && cartItem.map((item)=>(
-            <div className='cartContainer' key={item.product}>
-        <CartItemCard item={item}  deleteProductFCart={deleteProductFCart}/>
-        <div className='cartInput'>
-            <button onClick={()=> {return increaseProductCount(item.product,item.quantity,item.Stock)}}>+</button>
-            <input readOnly value={item.quantity}/>
-            <button onClick={()=> {return decreaseProductCount(item.product,item.quantity)}}>-</button>
-        </div>
-        <p className='cartSubtotal'>
-        {item.quantity*item.price}
-        </p>
-      </div>
-        ))
-      }
-      <div className='cartGrossProfit'>
-        <div></div>
-        <div className='cartGrossProfitBox'>
-        <p>Gross Profit</p>
-        <p>{`$500`}</p>
-        </div>
-        <div></div>
-        <div className='checkOutBtn'>
-            <button>Check Out</button>
-        </div>
-      </div>
-      </div>
-      </Fragment>
+      : <Fragment>
+        <div className="cartPage">
+          <div className="cartHeader">
+            <p>Product</p>
+            <p>Quantity</p>
+            <p>Subtotal</p>
+          </div>
+          {
+            cartItem && cartItem.map((item)=>(
+                <div className='cartContainer' key={item.product}>
+            <CartItemCard item={item}  deleteProductFCart={deleteProductFCart}/>
+            <div className='cartInput'>
+                <button onClick={()=> {return increaseProductCount(item.product,item.quantity,item.Stock)}}>+</button>
+                <input readOnly value={item.quantity}/>
+                <button onClick={()=> {return decreaseProductCount(item.product,item.quantity)}}>-</button>
+            </div>
+            <p className='cartSubtotal'>
+            {item.quantity*item.price}
+            </p>
+          </div>
+            ))
+          }
+          <div className='cartGrossProfit'>
+            <div></div>
+            <div className='cartGrossProfitBox'>
+            <p>Gross Profit</p>
+            <p>{`$500`}</p>
+            </div>
+            <div></div>
+            <div className='checkOutBtn'>
+                <button>Check Out</button>
+            </div>
+          </div>
+          </div>
+          </Fragment>
+    }
+   </Fragment>
   )
 }
 
