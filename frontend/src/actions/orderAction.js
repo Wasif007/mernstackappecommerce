5c428f721040ba8965c179869150843ad8c2fe38
Wasif007 <wasifateeq007@hotmail.com>
2023-09-29 00:15:27 +0500

    refactoring

---
+++ frontend/src/actions/orderAction.js
@@ -0,0 +1,26 @@
+import { ALL_ERROR_CLEAR, NEW_ORDER_FAIL, NEW_ORDER_REQUEST, NEW_ORDER_SUCCESS } from "../constants/orderConstant";
+
+  //Posting a order Action
+  export const resetPasswordMailUser = (order) => async (dispatch) => {
+    try {
+      dispatch({ type:  NEW_ORDER_REQUEST});
+      const config = { headers: { "Content-Type": "application/json" } };
+      const { data } = await axios.post(`/api/v1/order/new`,order,config);
+      dispatch({
+        type: NEW_ORDER_SUCCESS,
+        payload: data,
+      });
+    } catch (error) {
+      dispatch({
+        type: NEW_ORDER_FAIL,
+        payload: error.response.data.message,
+      });
+    }
+  };
+  
+  //Clearing all errors
+export const clearAllErrorFunc=()=>async(dispatch)=>{
+    dispatch({
+        type:ALL_ERROR_CLEAR
+    })
+    }
---
+++ frontend/src/reducers/orderReducer.js
@@ -0,0 +1,30 @@
+import { NEW_ORDER_FAIL, NEW_ORDER_REQUEST, NEW_ORDER_SUCCESS } from "../constants/orderConstant";
+
+//Order Placing reducer
+export const orderPlaceReducer = (state = { orderInfo: {} }, action) => {
+    switch (action.type) {
+      case NEW_ORDER_REQUEST:
+        return {
+            ...state,
+          loading: true,
+        };
+      case NEW_ORDER_SUCCESS:
+        return {
+          loading: false,
+          order: action.payload,
+        };
+      case  NEW_ORDER_FAIL:
+        return {
+          loading: true,
+          error: action.payload,
+        };
+  
+      case ALL_ERROR_CLEAR:
+        return {
+          ...state,
+          error: null,
+        };
+      default:
+        return state;
+    }
+  };
